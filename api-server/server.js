25960ed51f44bf5a6470225d88ce26df9f7f7fec
Devashish <kumardevashish0002gmai.com>
2026-06-18 06:46:54 +0530

    Implemented Docker Volumes for persistent database storage

--- api-server/server.js
+++ api-server/server.js
@@ -1,6 +1,45 @@
 const http = require("http");
 const os = require("os");
+const { Client } = require("pg");
 
-http.createServer((req,res)=>{
-    res.end(`Hello from ${os.hostname()}`);
-}).listen(3000,"0.0.0.0");
+const client = new Client({
+  host: "postgres",
+  user: "devashish",
+  password: "secret123",
+  database: "infra_lab",
+});
+
+client.connect()
+  .then(() => console.log("DB Connected"))
+  .catch(console.error);
+
+const server = http.createServer(async (req, res) => {
+console.log(req.url);
+  if (req.url === "/users") {
+    try {
+      const result = await client.query(
+        "SELECT * FROM users"
+      );
+
+      res.setHeader("Content-Type", "application/json");
+       res.end(
+    JSON.stringify({
+      container: os.hostname(),
+      users: result.rows
+    })
+  );
+    } catch (err) {
+      console.error(err);
+      res.statusCode = 500;
+      res.end("Database Error");
+    }
+
+    return;
+  }
+
+  res.end("API SERVER 🚀");
+});
+
+server.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
+});
